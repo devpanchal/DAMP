9ac74b458d3c9a9cffc3b40697ea98fc2c08cdea
unknown <devpanchal3@gmail.com>
2013-05-25 16:55:08 +0530

    Committed

--- DAMP_Data/DAMP_Data/Form1.cs
+++ DAMP_Data/DAMP_Data/Form1.cs
@@ -15,5 +15,10 @@ namespace DAMP_Data
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi");
+        }
     }
 }
